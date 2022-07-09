5aa509a47d38d5b7b7fb6069c81540948261a9ff
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-08 17:45:16 -0700

    maked some fixing to the positive program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,24 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdio.h>
 
 /**
  * main -assigns random to int n everytime 
  * it excutes , amd prints it
- * Return : Always 0 sucess
+ * Return: Always 0 (Sucess)
  **/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n=rand()-RAND_MAX / 2;
+	n = rand() - (RAND_MAX / 2);
 	if (n > 0)
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	else if (0 == n)
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	return (0);
 }
 
