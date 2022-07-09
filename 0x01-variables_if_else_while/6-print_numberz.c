887ab778e50076f4f5dc45439b9d6031a7ebdd13
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-09 14:51:42 -0700

    Created program 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all number from 10 to 0,
+ * followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 18; n < 28; n++)
+		putchar("%d", n);
+	putchar("\n");
+	return (0);
+}
