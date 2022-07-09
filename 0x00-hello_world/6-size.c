4f5e1c28e6a8c076531c6ba5c6000d6a85bca4b0
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-09 14:17:03 -0700

    Created the program 6-size.c that print the size of the various type variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	char d;
 	float f;
 	long int b;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of float: %lu byte(s)\n", sizeof(f));
+	printf("Size of float: %lu byte(s)\n", sizeof(int));
 	return (0);
 }
