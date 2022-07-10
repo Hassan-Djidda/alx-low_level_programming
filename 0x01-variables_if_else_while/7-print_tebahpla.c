2271c8a8b05d2273fdb03567563cba41accc2874
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-10 15:11:06 -0700

    created the program 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description - print all the alphabet in reverse  
+ * followed by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char achar;
+	
+	for (achar = 'z', achar >= 'a', achar++)
+	{
+		putchar(achar);
+	}
+	putchar('\n');
+}
+
+
