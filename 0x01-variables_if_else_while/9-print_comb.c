280b146026b61bd265535da5a26fc1778fa9796d
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-10 16:09:58 -0700

    Created the program 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - executed block
+ * Description: prints all possible combination of single digit number
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = '0'; chr <= '9'; chr++)
+	{
+		if (chr != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
