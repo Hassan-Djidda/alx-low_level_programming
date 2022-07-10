c0bc3b584f11293e729d41142fed829d44134d41
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-10 16:48:10 -0700

    Created the program 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ *main - main block to be excuted
+ *Description: print all possible combination of two digit
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int digit = 0;
+	int count;
+
+	while (digit < 10)
+	{
+		count = 0;
+		
+		while (count < 10)
+		{
+			if (count != digit && digit < count)
+			{
+				putchar('0' + digit);
+				putchar('0' + count);
+				if (count + digit != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		
+			}
+			count++;
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
