a27afa539c9312dbb9b05239a262b98aa65c1fb7
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-10 17:44:10 -0700

    Created the program 0-putchar.c

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+/**
+ * main - main block tobe executed
+ * Description: prints _putchar followed on new line
+ * Return: Alwats 0 sucess
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('h');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+	
