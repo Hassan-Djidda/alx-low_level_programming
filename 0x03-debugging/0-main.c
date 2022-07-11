4475978ad6a085acc7115471ebd6eda9fec94387
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-10 18:08:43 -0700

    Created the program 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints true if integer positive or not
+ * Return:Always 0 succes
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
