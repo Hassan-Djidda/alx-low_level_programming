2967d8c4eb48a51e5f3f6d26042b34726f6024df
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-12 16:17:14 -0700

    Created the directory and the 0-reset_to_98.c program

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * reset_to_98 - function that updatee the reference of a pointer
+ * @n: pointer to n be update
+ * Return: void
+ */
+void reset_to_98(int n){
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The pointer aray and stringis a project that help me to praticing my knowledge in array and string pointing derefrencing and much more
