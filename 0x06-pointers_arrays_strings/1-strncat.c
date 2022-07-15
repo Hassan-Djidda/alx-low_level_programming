1b8cfedb80a6ca7d708a4af69333abce7459d0a3
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 09:47:16 -0700

    added the strcat.c program

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * *_strncat - oncatenates n bytes of strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n :number of bytes
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_leng, i;
+
+	for (dest_leng = 0; dest[dest_leng] != '\0';  dest_leng++)
+		;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_leng + i] = src[i];
+
+	/*should end with end of string char */
+	dest[dest_leng + i] = '\0';
+
+	return (dest)
+}
