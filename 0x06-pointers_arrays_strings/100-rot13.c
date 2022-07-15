1d017075c41d89113e3063167d6cbf293e7b694c
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 16:08:34 -0700

    added the 100-rot13.c program

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * rot13 = encodes a string using rot13
+ * @s: the sring
+ * Return: the string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char boolean;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		boolean = 0;
+		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = r[i];
+				boolean = 1;
+			}
+		}
+	}
+	
+	return (s);
+}
