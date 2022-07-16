7f346fbd19fa7e78aa247e275511e3064bf4deb6
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 19:25:11 -0700

    aded main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alpahbet_x10(void);
+int _islower(int c);
+int _isalpha(intc c);
+int print_sign(int c);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+print_to_98(int n);
+void print_times_table(int n);
