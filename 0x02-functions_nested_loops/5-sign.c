ab3e5c8f45a3204d60af1d65feeacec330435c6f
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 19:53:05 -0700

    aded 4-isalpha.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
+ */
+int print_sign(int n)
+{
+	int numz;
+
+	if (n > 0)
+	{
+		numz = 1;
+		putchar('+');
+	}
+	else if (n == 0)
+	{
+		numz = 0;
+		putchar('0');
+	}
+	else
+	{
+		numz = -1;
+		putchar('-');
+	}
+	return (numz);
