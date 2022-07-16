bdd4e98a82255d53aec85cd78cd542f4b573161a
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 19:14:53 -0700

    aded 2-largest_number.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
