d24da4cad31433613b46e8eb53b88d37c08c9bed
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 17:46:59 -0700

    added an error the 7-puts_half.c  program

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,18 +15,18 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	if (len % 2 === 0)
+	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			putchar(str[1]);
+			putchar(str[i]);
 		}
 	}
 	else if (len % 2)
 	{
 		for (n = (len -1) / 2; n < len - 1; n++)
 		{
-			putchar(str[n+1]);
+			putchar(str[n + 1]);
 		}
 	}
 	putchar('\n');
