22db06d1ee524d91bfc17d7c4092952aab9b1970
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-15 20:11:12 -0700

    aded 4-isalpha.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: used for the arguments of the function
+ * Return: integer value
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	last = last * -1;
+	putchar(last + '0');
+	return (last);
+}
