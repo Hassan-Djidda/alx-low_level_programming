2b77def4eb7860c7522c18bc70eef9dbfeab2554
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2022-07-22 06:02:56 -0700

    added the 0-isupper.c program

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: an integer as a parameter
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return(0);
+}
