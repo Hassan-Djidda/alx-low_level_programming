688bff09cd82d067a21cf78efac2170309ec7576
Hassan-Djidda <hassanmahamatdjidda47@gmail.com>
2023-03-23 15:23:43 -0700

    Fix some bugs

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,15 @@ void  print_diagonal(int n)
 	int position, space;
 	
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (position = 1; position <= n; ++position)
 		{
 			for (space = 1; space <= position; ++space)
-				putchar(' ');
-			putchar(92);
-			putchar('\n');
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }
