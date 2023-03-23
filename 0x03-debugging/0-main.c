fd8eb92a06d17c2f86f901e5d395c0aad5322070
Hassan-Djidda <hassanmahamatdjidda47@gmail.com>
2023-03-23 06:44:12 -0700

    Debugged the 0-main.c Program

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - Function that prints if integer is positive or nagative.
- *
- * Return: Always 0 (successful)
-*/
-
+ * main - tests function that prints integer is positive or negative.
+ * Return: 0
+ */
 int main(void)
 {
 	int i;
 
-	i = 0;
-	positive_or_nagative(i);
+	i = 50 % 2;
+	positive_or_negative(i);
 
 	return (0);
 }
