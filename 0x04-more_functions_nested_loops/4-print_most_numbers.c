1dd2022f60eef8dff3a27feea46cce40e5a8d9fa
Hassan-Djidda <hassanmahamatdjidda47@gmail.com>
2023-03-23 09:56:43 -0700

    Add the 4-print_most_numbers.c program

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 0 except 2, 4.
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		if (ch == '2' || ch == '4')
+			continue;
+		else
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
