e91a4e356e2200c005d3c99279925fd4b7b75924
Hassan-Djidda <hassanmahamatdjidda47@gmail.com>
2023-03-23 13:30:56 -0700

    Add the 5-more_numbers.c program

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the number, from 1 to 14
+ * 		followed by a new line
+ * Retrun: void
+ */
+void more_numbers(void)
+{
+	char ch = '0';
+	int i;
+
+	i = 0;
+	while (i < =10)
+	{
+		while (ch <= '14')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		i++;
+	}
+}
