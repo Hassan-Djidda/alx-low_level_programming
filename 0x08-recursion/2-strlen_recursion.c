049914032dea68347414e5c41b1cf6be4e01341d
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-30 09:20:33 -0700

    Fix main.h and 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s)
 	int length = 0;
 
 	if (*s != '\0')
-		return (lenght + _strlen_recursion(s + 1));
+		return (length + _strlen_recursion(s + 1));
 	else
 		return (0);	
 }
