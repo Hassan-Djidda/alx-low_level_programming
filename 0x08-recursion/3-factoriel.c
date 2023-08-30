c5a144c22527a5c78156b3fa4e3e92698f3ae827
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-30 10:08:38 -0700

    Add 3-factoriel.c file

---
+++ 0x08-recursion/3-factoriel.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - return factoriel of a givin number
+ *
+ * @n: number
+ *
+ * Return: num
+ */
+
+int factorial(int n);
+{
+	if (n < 0)
+		return -1;
+	else if (n == 0 || n == 1)
+		return 1;
+	else
+		return factoriel(n - 1) + factoriel(n - 2);
+}
