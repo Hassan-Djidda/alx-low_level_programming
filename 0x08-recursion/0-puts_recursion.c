1f7e2ef4f87b7b5c98bf2cdd6fd3408949235e6a
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-30 07:53:52 -0700

    Add main.h and 0-puts_recusion file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a given string pointer
+ *
+ * @s - pointer of the string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
