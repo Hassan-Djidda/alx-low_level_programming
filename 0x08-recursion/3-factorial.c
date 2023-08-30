3c753decccba16a8c7b046b0d1991ee79d9afd62
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-30 10:19:34 -0700

    Fix bug 3-factorial.c file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,8 +12,8 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
+	else if (n == 0)
 		return (1);
 	else
-		return (factorial(n - 1) + factorial(n - 2));
+		return (n * factorial(n - 1));
 }
