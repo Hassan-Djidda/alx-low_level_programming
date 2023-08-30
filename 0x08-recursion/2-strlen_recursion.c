21ae43899faf2da62a04d26347f36baffd0b5196
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-30 09:17:41 -0700

    Fix bug 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,9 +13,7 @@ int _strlen_recursion(char *s)
 	int length = 0;
 
 	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-		length++;
-	
-	}
+		return (lenght + _strlen_recursion(s + 1));
+	else
+		return (0);	
 }
