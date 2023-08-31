7fecf7cc75e1ed1e45de0c06f2b1fa5573647fd2
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-31 06:12:27 -0700

    fix Bug in 5-sqrt_recursion.c file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,14 +17,13 @@ int _sqrt_recursion(int n)
 	if (root < 0)
 		return (-1);
 	else
-		return(sqrt_check(root + 1`, number); 
+		return(sqrt_check(root + 1, n); 
 }
 
 int sqrt_check(int root, int number)
 {	
-
-		if ((root * root) > number)
-			return (-1);
-		if ((root * root) == number)
-			return root;
+	if ((root * root) > number)
+		return (-1);
+	if ((root * root) == number)
+		return root;
 }
