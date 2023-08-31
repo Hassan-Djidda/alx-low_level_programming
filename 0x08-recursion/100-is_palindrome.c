c0e278a3a1e0c9ddc79a71e8544854a5f83ff4bc
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-08-31 11:39:40 -0700

    Add 100-is_palindrome.c file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,10 +10,10 @@
 int _strlen_recursion(char *s)
 {
 	int length = 0;
-	if (s != '\0')
+	if (*s != '\0')
 	{
 		length++;
-		length += strlen_recursion(s + 1);
+		length += _strlen_recursion(s + 1);
 	}
 	return (length);
 }
