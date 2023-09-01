a12643b0f84ef9f64cfddf5b77981a667389968c
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-01 11:49:23 -0700

    Fix bugs in 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0 success
  */
 
-int main(int __attribute__ ((unused)) argc, char **argv[])
+int main(int argc, char **argv[])
 {
-	(void) argc;
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
