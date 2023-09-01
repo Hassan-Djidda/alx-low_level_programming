d2615e8d78f6ff9ebedf09bf3c8ea248527e47b4
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-01 12:03:51 -0700

    Add the 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of argument
+ * @argv: arrays of arguments
+ *
+ * Return: always (0)
+ */
+int main(int argc, char __attribute__((unused))  *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
+
