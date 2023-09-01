e077e3e3fa03a9ec738304a4f431938843e583a7
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-01 13:02:29 -0700

    Add 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the sum of arguments followed by new linw
+ * @argc: number of arguments
+ * @argv: arryays of string arguments
+ *
+ * Return: 0 succcess
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	int sum, i = 0;
+	if (argc == 1)
+		print("0\n");
+	for (i = 0; i < argc; i++)
+		if (
+		printf("%s\n", argv[i]);
+
+	return (0);
