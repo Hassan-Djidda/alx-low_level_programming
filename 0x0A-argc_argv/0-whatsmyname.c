ed9ba735573dbbe72aab8d5a966916f1411bee84
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-01 11:41:32 -0700

    Add main.h and fix bux in 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new linw
+ * @argc: number of arguments
+ * @rgv: arrays of arguments
+ *
+ * Return: 0 success
+ */
+
+int main(int __attribute__ ((unused)) argc, char **argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
