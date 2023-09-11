cb583fe1e34de0e8888c67c008e2a61545150b4f
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-11 07:35:14 -0700

    Fix bug in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,28 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define  struct dog {char *name, float age, char *owner}
+/**
+ * struct dog - define a dog structure
+ * @name: name of the  * @age: age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_type - type definition for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
