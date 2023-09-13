6b18d057706b681da184e58dd48a99e9de822aec
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-13 08:14:14 -0700

    fix: /3-calc.h /3-get_op_func.c /3-op_functions /3-main.c fix error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", oprt(atoit(argv[1], atoi(argv[3]))));
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
