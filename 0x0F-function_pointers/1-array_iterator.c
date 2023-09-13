73aba228c18f89f55dc83035d28caadf7dece1f7
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-09-13 06:19:48 -0700

    bulid: create 1-array_iterator file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator: a  function excutes a function in each element of the array
+ * @array: an array to perform the iteration on
+ * @size: size of the array
+ * @action: is pointer to function you need to use
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(arrar[i]);
+			i++;
+		}
+	}
+}
+
