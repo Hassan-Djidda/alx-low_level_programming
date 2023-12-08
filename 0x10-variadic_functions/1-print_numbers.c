d13a825c09a3e07271915f0c2418e01d275d5b42
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2023-12-08 02:32:12 -0800

    Add 1-print_numbers.c program

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include "variadic_function.s"
+
+/**
+ * print_numbers - a function that print numbers, follewed by a new line
+ *
+ * @seperator: pointer to a constant seperator
+ * @n: start of the argument
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int idx;
+
+	va_start(ap, n);
+
+	for (idx = 0; idx < n; idx++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (seperator && index != n-1)
+			printf("%s", seperator);
+	}
+	va_end(ap);
+	printf("\n");
+}
