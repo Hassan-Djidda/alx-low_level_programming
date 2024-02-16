45e110e177e31eac0c9ae3e715fac1c0439bbb6c
Hassan Mahamat Djidda <hassanmahamatdjidda47@gmail.com>
2024-02-16 12:17:04 -0800

    Fix bug in 2-print_strings.c program0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,5 @@
 #include <stdarg.h>
-#include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * print_strings - a function that prints strings, followed by a new line
@@ -9,4 +9,27 @@
  *
  * Return: Void
  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int idx;
+	char *str;
 
+	va_start(ap, n);
+
+	for (idx = 0; idx < n; idx++)
+	{
+
+		str = va_arg(ap, *char);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+	
+		if (separator && index != n - 1)
+			printf("%s", separator);
+	}
+	
+	va_end(ap);
+	printf("\n");
+}
